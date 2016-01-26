b943d792989f1ee3b72c878d1e7bf315573d0bda
Chris Wynne <chris.wynndow@gmail.com>
2016-01-26 16:56:05 +0000

    Adds API and tests with httpBackend

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+
+  var self = this;
+  var searchResource = $resource('https://api.github.com/search/users');
+
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm,
+        access_token: gitAccessToken }
+    );
+  };
 
 }]);
