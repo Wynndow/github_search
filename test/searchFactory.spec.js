2f95935d60ed49af07de2862ef75ddb19a49af83
Chris Wynne <chris.wynndow@gmail.com>
2016-01-28 13:55:11 +0000

    Moves test array out of searchFactory.spec.js

--- test/searchFactory.spec.js
+++ test/searchFactory.spec.js
@@ -8,18 +8,6 @@ describe('factory: Search', function() {
     search = Search;
   }));
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
 
   beforeEach(inject(function($httpBackend) {
     httpBackend = $httpBackend;
